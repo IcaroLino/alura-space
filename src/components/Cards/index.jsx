cf0aba2662d0aebb22194163c0b26eac2cfb7ee2
Icaro <luizicaro.s@gmail.com>
2023-03-28 23:09:37 -0300

    Refactoring list of Card component

--- src/components/Cards/index.jsx
+++ src/components/Cards/index.jsx
@@ -4,13 +4,7 @@ import Card from '../Card';
 export default function Cards({ pictures, styles }) {
   return (
     <ul className={styles.gallery__cards}>
-    {
-      pictures.map((picture) => {
-        return (
-          <Card key={picture.id} picture={picture} styles={styles} />
-        );
-      })
-    }
+      {pictures.map((picture) => <Card key={picture.id} picture={picture} styles={styles} />)}
     </ul>
   )
 }
