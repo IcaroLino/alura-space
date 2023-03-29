08b51ed4c9678affa2a967bbf6b318104fbc1a58
Icaro <luizicaro.s@gmail.com>
2023-03-29 15:48:52 -0300

    Adding filtering to Tags component

--- src/components/Gallery/index.jsx
+++ src/components/Gallery/index.jsx
@@ -1,16 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Tags from '../Tags'
 import pictures from './pictures.json';
 import styles from './Gallery.module.scss';
 import Cards from '../Cards';
 
 export default function Gallery() {
+  const [items, setItems] = useState(pictures);
+  const tags = Array.from(new Set(pictures.map((picture) => picture.tag)));
+
+  function filterPictures(tag) {
+    tag === 'all'
+      ? setItems(pictures)
+      : setItems(pictures.filter((picture) => picture.tag === tag));
+  }
+ 
   return (
     <section className={styles.gallery}>
       <h2>Navegue pela galeria</h2>
-      <Tags />
-      <Cards pictures={pictures} styles={styles} />            
+      <Tags items={tags} filter={filterPictures} />
+      <Cards pictures={items} styles={styles} />            
     </section>
-
   )
 }
--- src/components/Tags/index.jsx
+++ src/components/Tags/index.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import styles from './Tags.module.scss';
 
-export default function Tags() {
+export default function Tags({ items, filter }) {
   return (
     <div className={styles.tags}>
       <p>Filtre por tags:</p>
       <ul className={styles.tags__list}>
-        <li>Estrelas</li>
-        <li>Galáxias</li>
-        <li>Lua</li>
-        <li>Planetas</li>
+        {items.map((tag) => <li key={tag} onClick={() => filter(tag)} >{tag}</li> )}
+        <li onClick={() => filter('all')} >Todas</li>
       </ul>
     </div>
   )
